3f3823f0504189a4ef2b6b9525699728d31e4ddc
ANURUDDH <hellomoto.1769@gmail.com>
2022-02-16 11:55:40 +0530

    pramp

---
+++ Validate-IP-Address.js
@@ -0,0 +1,11 @@
+function validateIP(ip) {
+	/**
+	@param ip: string
+	@return: boolean
+	*/
+
+const regexExp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+
+  return regexExp.test(ip);
+  
+}
